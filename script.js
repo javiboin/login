33a98bb2b98a261a1067b257cba11e033103d9b9
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2025-09-01 00:53:34 -0300

    Add: Funcionalidades en JavaScript

---
+++ script.js
@@ -0,0 +1,86 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('loginForm');
+    const emailInput = document.getElementById('email');
+    const emailError = document.getElementById('emailError');
+    const passwordInput = document.getElementById('password');
+    const passwordError = document.getElementById('passwordError');
+    const confirmPasswordInput = document.getElementById('confirmPassword');
+    const confirmPasswordError = document.getElementById('confirmPasswordError');
+
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        validateForm();
+    })
+
+    emailInput.addEventListener('blur', function() {
+        validateEmail();
+    })
+
+    emailInput.addEventListener('change', function() {
+        clearError(emailError);
+    })
+
+    passwordInput.addEventListener('change', function() {
+        clearError(passwordError);
+    })
+
+    confirmPasswordInput.addEventListener('change', function() {
+        clearError(confirmPasswordError);
+    })
+
+    function validateForm() {
+        const isValidEmail = validateEmail();
+        const isValidPassword = validatePassword();
+        const isPasswordMatch = passwordMatch();
+
+        if (isValidEmail && isValidPassword && isPasswordMatch) {
+            // guardar email en el localStorage y generar un JSON en consola
+            alert('Formulario enviado correctamente');
+        }
+    }
+
+    function validateEmail() {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const emailValue = emailInput.value.trim();
+
+        if(!emailRegex.test(emailValue)) {
+            showError(emailError, 'Ingrese un email valido');
+            return false;
+        };
+
+        return true;
+    }
+
+    function validatePassword() {
+        const passwordValue = passwordInput.value.trim();
+
+        if (passwordValue.length < 6) {
+            showError(passwordError, 'Ingrese una contraseña de al menos 6 caracteres');
+            return false;
+        }
+
+        return true;
+    }
+
+    function passwordMatch() {
+        const passwordValue = passwordInput.value.trim();
+        const confirmPasswordValue = confirmPasswordInput.value.trim();
+
+        if (passwordValue != confirmPasswordValue) {
+            showError(confirmPasswordError, 'Las contraseñas no coiniden');
+            return false;
+        }
+
+        return true;
+    }
+
+    function showError (errorElement, message) {
+        errorElement.innerHTML = message;
+        errorElement.style.display = 'block';
+    }
+
+    function clearError (errorElement) {
+        errorElement.innerHTML = '';
+        errorElement.display = 'none';
+    }
+});
